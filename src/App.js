df88b6f7d621eaf28b8c275dcbab48f647f7af88
Muskan <muskangupta9566@gmail.com>
2024-06-13 18:06:57 +0530

    commit_code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import XTable from "./date";
+
+function App() {
+  return (
+    <div className="App">
+      <XTable/>
+    </div>
+  );
+}
+
+export default App;
